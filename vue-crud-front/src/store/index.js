4e6b14d33e2cc24a4244281de3119d2bcd0ca050
Rodrigo Iván Miranda Reidenbach <rodrigomreidenbach@gmail.com>
2023-10-03 18:11:41 -0300

    fixing kubernetes vue app config

--- vue-crud-front/src/store/index.js
+++ vue-crud-front/src/store/index.js
@@ -23,12 +23,20 @@ export default createStore({
   actions: {
     async fetchToken({ commit,state }) {
       try {
-        console.log("VARIABLE DE ENTORNO URL=",process.env.URL)
-      state.isLoading.authenticate=true
-        const response = await axios.get(`${process.env.URL}/authenticate`);
-        const token = response.data.token
-        commit('setToken', token);
-        state.isLoading.authenticate=false
+        if(process.env.ENTORNO == 'localhost'){
+          state.isLoading.authenticate=true
+            const response = await axios.get(`http://localhost:8088/authenticate`);
+            const token = response.data.token
+            commit('setToken', token);
+            state.isLoading.authenticate=false
+        }else if(process.env.ENTORNO == 'kubernetes'){
+          state.isLoading.authenticate=true
+            const response = await axios.get(`http://af51824567d1941d08c1e31f3e8dce06-1892599787.sa-east-1.elb.amazonaws.com:5000/authenticate`);
+            const token = response.data.token
+            commit('setToken', token);
+            state.isLoading.authenticate=false
+        }
+
       } catch (error) {
       
         state.isLoading.authenticate=false
@@ -38,22 +46,35 @@ export default createStore({
       }
     },
     async fetchUser({ commit, state }, user) {
-
-  
           try {
+
             const config = {
               headers: {
                 Authorization: `Bearer ${state.token}`
               }
             }
-            console.log(user)
-            state.isLoading.getUserByName = true;
-            const url = `http://${process.env.URL}/getUserByName/${user}`; 
-            const response = await axios.get(url,config);
-            const responseUser = response.data.user;
-            state.isLoading.getUserByName = false;
-            console.log("response user",responseUser)
-            return responseUser
+            if(process.env.ENTORNO == 'localhost'){
+
+              console.log(user)
+              state.isLoading.getUserByName = true;
+              const url = `http://localhost:8088/getUserByName/${user}`; 
+              const response = await axios.get(url,config);
+              const responseUser = response.data.user;
+              state.isLoading.getUserByName = false;
+              console.log("response user",responseUser)
+              return responseUser
+            }else if(process.env.ENTORNO == 'kubernetes'){
+
+              console.log(user)
+              state.isLoading.getUserByName = true;
+              const url = `http://af51824567d1941d08c1e31f3e8dce06-1892599787.sa-east-1.elb.amazonaws.com:5000/getUserByName/${user}`; 
+              const response = await axios.get(url,config);
+              const responseUser = response.data.user;
+              state.isLoading.getUserByName = false;
+              console.log("response user",responseUser)
+              return responseUser
+            }
+    
           } catch (error) {
             
         console.log("ERROR:",error)
@@ -73,13 +94,25 @@ export default createStore({
                 Authorization: `Bearer ${state.token}`
               }
             }
-            state.isLoading.getUsers = true;
-            const url = `http://${process.env.URL}/getUsers`; // Concatena user en la URL
-            const response = await axios.get(url,config);
-            const responseUser = response.data.users;
-            state.isLoading.getUsers = false;
-            console.log("response user",responseUser)
-            return responseUser
+            if(process.env.ENTORNO == 'localhost'){
+
+              state.isLoading.getUsers = true;
+              const url = `http://localhost:8088/getUsers`; // Concatena user en la URL
+              const response = await axios.get(url,config);
+              const responseUser = response.data.users;
+              state.isLoading.getUsers = false;
+              console.log("response user",responseUser)
+              return responseUser
+            }else if(process.env.ENTORNO == 'kubernetes'){
+
+              state.isLoading.getUsers = true;
+              const url = `http://af51824567d1941d08c1e31f3e8dce06-1892599787.sa-east-1.elb.amazonaws.com:5000/getUsers`; // Concatena user en la URL
+              const response = await axios.get(url,config);
+              const responseUser = response.data.users;
+              state.isLoading.getUsers = false;
+              console.log("response user",responseUser)
+              return responseUser
+            }
           } catch (error) {
             
         console.log("ERROR:",error)
@@ -98,14 +131,27 @@ export default createStore({
             Authorization: `Bearer ${state.token}`
           }
         }
-        console.log(user)
-        state.isLoading.deleteUser = true;
-        const url = `http://${process.env.URL}/deleteUser/${user}`; // Concatena user en la URL
-        const response = await axios.delete(url,config);
-        const responseUser = response.data.delete;
-        state.isLoading.deleteUser = false;
-        console.log("response user",responseUser)
-        return responseUser
+        if(process.env.ENTORNO == 'localhost'){
+          console.log(user)
+          state.isLoading.deleteUser = true;
+          const url = `http://localhost:8088/deleteUser/${user}`; // Concatena user en la URL
+          const response = await axios.delete(url,config);
+          const responseUser = response.data.delete;
+          state.isLoading.deleteUser = false;
+          console.log("response user",responseUser)
+          return responseUser
+        }else if(process.env.ENTORNO == 'kubernetes'){
+          console.log(user)
+          state.isLoading.deleteUser = true;
+          const url = `http://af51824567d1941d08c1e31f3e8dce06-1892599787.sa-east-1.elb.amazonaws.com:5000/deleteUser/${user}`; // Concatena user en la URL
+          const response = await axios.delete(url,config);
+          const responseUser = response.data.delete;
+          state.isLoading.deleteUser = false;
+          console.log("response user",responseUser)
+          return responseUser
+        }
+
+
       } catch (error) {
         
     console.log("ERROR:",error)
@@ -115,19 +161,32 @@ export default createStore({
   
 },
     async fetchInsertUser({ commit, state }, data) { 
+      
           try {
             const config = {
               headers: {
                 Authorization: `Bearer ${state.token}`
               }
             }
-            state.isLoading.insertUser = true;
-            const url = `http://${process.env.URL}/insertUser`; // Concatena user en la URL
-            const response = await axios.post(url,{name:data.nombre,age:data.edad},config);
-            const responseUser = response.data.insert;
-            state.isLoading.insertUser = false;
-            console.log("response user",responseUser)
-            return responseUser
+            if(process.env.ENTORNO == 'localhost'){
+              state.isLoading.insertUser = true;
+              const url = `http://localhost:8088/insertUser`; // Concatena user en la URL
+              const response = await axios.post(url,{name:data.nombre,age:data.edad},config);
+              const responseUser = response.data.insert;
+              state.isLoading.insertUser = false;
+              console.log("response user",responseUser)
+              return responseUser
+            }else if(process.env.ENTORNO == 'kubernetes')
+            {
+              state.isLoading.insertUser = true;
+              const url = `http://af51824567d1941d08c1e31f3e8dce06-1892599787.sa-east-1.elb.amazonaws.com:5000/insertUser`; // Concatena user en la URL
+              const response = await axios.post(url,{name:data.nombre,age:data.edad},config);
+              const responseUser = response.data.insert;
+              state.isLoading.insertUser = false;
+              console.log("response user",responseUser)
+              return responseUser
+            }
+
           } catch (error) {
             
         console.log("ERROR:",error)
